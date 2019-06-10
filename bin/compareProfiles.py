fca6ef58e48eb5d368cc777e556cd776744667f4
David Eyre <david.eyre@bdi.ox.ac.uk>
2019-06-10 09:43:31 +0100

    fix typo

--- bin/compareProfiles.py
+++ bin/compareProfiles.py
@@ -5,11 +5,6 @@ from optparse import OptionParser
 
 
 def compareHash(input_folder, compareOut):
-import time
-
-
-
-print(end - start)
 	fileList = glob.glob('%s/*/*.json'%input_folder)
 	w = open(compareOut, 'w')
 	w.write('id1\tid2\tloci_compared\tdifferences\tdist\n')
