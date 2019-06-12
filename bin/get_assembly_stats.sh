6734f6b7f088b4d0d94244d2dab734211f1035a6
David Eyre <david.eyre@bdi.ox.ac.uk>
2019-06-12 11:10:05 +0100

    make summary output scripts more generalised

--- bin/get_assembly_stats.sh
+++ bin/get_assembly_stats.sh
@@ -1,4 +1,12 @@
-tail -n 1 -q replicates_output/*/*_cgmlst.statlog  > tmp_assembly_stats.txt
-cat replicates_output/*/*_cgmlst.statlog | head -1 > tmp_header.txt
+#run in root output folder, e.g. cd comparison_study_data/replicates_output
+# get assembly stats
+tail -n 1 -q */*_cgmlst.statlog  > tmp_assembly_stats.txt
+cat */*_cgmlst.statlog | head -1 > tmp_header.txt
 cat tmp_header.txt tmp_assembly_stats.txt > assembly_stats.txt
 rm tmp_assembly_stats.txt tmp_header.txt
+
+#get mlst
+tail -n 1 -q */*_mlst.txt  > tmp_mlst.txt
+cat */*_mlst.txt | head -1 > tmp_header.txt
+cat tmp_header.txt tmp_mlst.txt > mlst_summary.txt
+rm tmp_mlst.txt tmp_header.txt
