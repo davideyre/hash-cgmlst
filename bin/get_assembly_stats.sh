5aa036bf8b7d5d9de65b9b850504905dc43e8d7c
David Eyre <davideyre@gmail.com>
2019-08-30 14:47:07 +0100

    exclude skesa from main assembly stats

--- bin/get_assembly_stats.sh
+++ bin/get_assembly_stats.sh
@@ -1,7 +1,7 @@
 #run in root output folder, e.g. cd comparison_study_data/replicates_output
 # get assembly stats
-tail -n 1 -q */*_cgmlst.statlog  > tmp_assembly_stats.txt
-cat */*_cgmlst.statlog | head -1 > tmp_header.txt
+tail -n 1 -q */*spades_cgmlst.statlog  > tmp_assembly_stats.txt
+cat */*spades_cgmlst.statlog | head -1 > tmp_header.txt
 cat tmp_header.txt tmp_assembly_stats.txt > assembly_stats.txt
 rm tmp_assembly_stats.txt tmp_header.txt
 
